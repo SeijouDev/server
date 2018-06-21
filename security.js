b565c60ca55c97e50692ebb87cb8b82d49063b44
ESTUDIANTES <alejo.1395@hotamail.com>
2018-06-20 19:56:50 -0500

    update

--- security.js
+++ security.js
@@ -1,11 +1,13 @@
 var express = require('express');   
 var app = express();
 
+var port = process.env.PORT || 8000;
+
 app.get('/',  (req, res) => {
   res.json({status: 200, message: 'Ya conecto!'});
 });
 
 
-app.listen(3000,  () =>  {
-  console.log('Example app listening on port 3000!');
+app.listen(port,  () =>  {
+  console.log('Example app listening on port '+port+'!');
 });
